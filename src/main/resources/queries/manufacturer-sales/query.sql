baedf3f72366a6f1a61a4dfaa177b3c65664c859
Daniel Lima <daniel.santos@hipersaude.com.br>
2026-08-24 22:17:10 -0300

    refactor(manufacturer-sales): lista colunas explícitas em vez de SELECT *

--- src/main/resources/queries/manufacturer-sales/query.sql
+++ src/main/resources/queries/manufacturer-sales/query.sql
@@ -1,3 +1,8 @@
-SELECT *
+SELECT
+  ean,
+  productName,
+  totalQuantity,
+  totalAmount,
+  totalOrders
 FROM `rm-farma-dw-prod.licenciado.get_manufacturer_sales_by_period`(@cnpj, @manufacturerPattern, @startDate, @endDate)
 ORDER BY totalAmount DESC
