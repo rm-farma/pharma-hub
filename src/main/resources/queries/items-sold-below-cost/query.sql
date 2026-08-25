3efc4216e04e762c7d32e5e2a4913454f60223d5
Daniel Lima <daniel.santos@hipersaude.com.br>
2026-08-24 22:16:58 -0300

    refactor(items-sold-below-cost): lista colunas explícitas em vez de SELECT *

--- src/main/resources/queries/items-sold-below-cost/query.sql
+++ src/main/resources/queries/items-sold-below-cost/query.sql
@@ -1,3 +1,10 @@
-SELECT *
+SELECT
+  ean,
+  productName,
+  notaFiscal,
+  dataVenda,
+  totalQuantity,
+  faturamento,
+  custo
 FROM `rm-farma-dw-prod.licenciado.get_items_sold_below_cost`(@cnpj, @startDate, @endDate)
 ORDER BY dataVenda DESC
