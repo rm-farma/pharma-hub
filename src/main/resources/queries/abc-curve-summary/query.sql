248faaf637023f1e561c63b56ae885bf6b0218cf
Daniel Lima <daniel.santos@hipersaude.com.br>
2026-08-24 22:16:47 -0300

    refactor(abc-curve-summary): lista colunas explícitas em vez de SELECT *

--- src/main/resources/queries/abc-curve-summary/query.sql
+++ src/main/resources/queries/abc-curve-summary/query.sql
@@ -1,2 +1,10 @@
-SELECT *
+SELECT
+  total_produtos,
+  total_produtos_a,
+  total_produtos_b,
+  total_produtos_c,
+  faturamento_total,
+  faturamento_a,
+  faturamento_b,
+  faturamento_c
 FROM `rm-farma-dw-prod.licenciado.get_abc_curve_summary`(@cnpj, @startDate, @endDate)
