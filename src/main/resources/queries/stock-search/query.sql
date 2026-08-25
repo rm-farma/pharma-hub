d0fc46c925ada456ab18a68042871642411e4732
Daniel Lima <daniel.santos@hipersaude.com.br>
2026-08-24 22:19:20 -0300

    refactor(stock-search): lista colunas explícitas em vez de SELECT *

--- src/main/resources/queries/stock-search/query.sql
+++ src/main/resources/queries/stock-search/query.sql
@@ -1,3 +1,11 @@
-SELECT *
+SELECT
+  ean,
+  apresentacao,
+  fabricante,
+  grupo_macro,
+  saldo_estoque,
+  custo_medio,
+  custo_medio_total,
+  preco_venda
 FROM `rm-farma-dw-prod.licenciado.get_stock_by_search`(@cnpj, @searchTerm)
 ORDER BY apresentacao ASC
