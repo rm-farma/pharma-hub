af728448f80e8e7fb05e76016270725d6d98f6f2
Daniel Lima <daniel.santos@hipersaude.com.br>
2026-08-24 22:16:47 -0300

    refactor(idle-stock): lista colunas explícitas em vez de SELECT *

--- src/main/resources/queries/idle-stock/query.sql
+++ src/main/resources/queries/idle-stock/query.sql
@@ -1,3 +1,14 @@
-SELECT *
+SELECT
+  ean,
+  apresentacao,
+  fabricante,
+  grupo_macro,
+  saldo_estoque,
+  custo_medio_total,
+  preco_venda,
+  total_skus,
+  total_unidades,
+  valor_total_custo,
+  valor_total_venda
 FROM `rm-farma-dw-prod.licenciado.get_idle_stock`(@cnpj)
 ORDER BY custo_medio_total DESC
