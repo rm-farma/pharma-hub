ba744b44d1c360b4693d1cd39600e17ede449762
Daniel Lima <daniel.santos@hipersaude.com.br>
2026-08-24 22:16:47 -0300

    refactor(sales-comparison): lista colunas explícitas em vez de SELECT *

--- src/main/resources/queries/sales-comparison/query.sql
+++ src/main/resources/queries/sales-comparison/query.sql
@@ -1,2 +1,13 @@
-SELECT *
+SELECT
+  periodoBase,
+  periodoComparado,
+  faturamentoBase,
+  faturamentoComparado,
+  variacaoFaturamento,
+  itensVendidosBase,
+  itensVendidosComparado,
+  variacaoItensVendidos,
+  ticketMedioBase,
+  ticketMedioComparado,
+  variacaoTicketMedio
 FROM `rm-farma-dw-prod.licenciado.compare_sales_periods`(@cnpj, @startDate1, @endDate1, @startDate2, @endDate2)
