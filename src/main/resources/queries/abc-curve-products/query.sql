3cf57dd91b64378c5a678cd06d011841d68096fa
Daniel Lima <daniel.santos@hipersaude.com.br>
2026-08-24 22:16:47 -0300

    refactor(abc-curve-products): lista colunas explícitas em vez de SELECT *

--- src/main/resources/queries/abc-curve-products/query.sql
+++ src/main/resources/queries/abc-curve-products/query.sql
@@ -1,3 +1,14 @@
-SELECT *
+SELECT
+  ean,
+  apresentacao,
+  faturamento_total,
+  quantidade_vendida,
+  num_transacoes,
+  percentual_individual,
+  percentual_acumulado,
+  classe_abc,
+  saldo_estoque,
+  preco_venda,
+  custo_medio
 FROM `rm-farma-dw-prod.licenciado.get_abc_curve_products`(@cnpj, @startDate, @endDate, @classeAbc)
 ORDER BY faturamento_total DESC
