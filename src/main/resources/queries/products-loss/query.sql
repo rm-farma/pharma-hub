f0cc6b5767191cb39c0ae7c643210edcf22a176f
Daniel Lima <daniel.santos@hipersaude.com.br>
2026-08-24 22:17:28 -0300

    refactor(products-loss): lista colunas explícitas em vez de SELECT *

--- src/main/resources/queries/products-loss/query.sql
+++ src/main/resources/queries/products-loss/query.sql
@@ -1,3 +1,8 @@
-SELECT *
+SELECT
+  ean,
+  productName,
+  totalQuantity,
+  faturamento,
+  custo
 FROM `rm-farma-dw-prod.licenciado.get_products_loss_by_period`(@cnpj, @startDate, @endDate)
 ORDER BY (custo - faturamento) DESC
