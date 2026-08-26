c902686eb36ac0b34e7052e3af715279e4436443
Daniel Lima <daniel.santos@hipersaude.com.br>
2026-08-26 09:48:24 -0300

    fix(bigquery): corrige dataset da table function em stock-without-sales para rmfarma.ISAZ

--- src/main/resources/queries/stock-without-sales/query.sql
+++ src/main/resources/queries/stock-without-sales/query.sql
@@ -6,5 +6,5 @@ SELECT
   saldo_estoque,
   custo_medio_total,
   preco_venda
-FROM `rm-farma-dw-prod.licenciado.get_stock_without_sales`(@cnpj)
+FROM `rmfarma.ISAZ.get_stock_without_sales`(@cnpj)
 ORDER BY custo_medio_total DESC
