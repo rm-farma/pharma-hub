b466dde71b00ae802bd078c84813438767203cb2
Daniel Lima <daniel.santos@hipersaude.com.br>
2026-08-26 09:48:24 -0300

    fix(bigquery): corrige dataset da table function em stock-metrics para rmfarma.ISAZ

--- src/main/resources/queries/stock-metrics/query.sql
+++ src/main/resources/queries/stock-metrics/query.sql
@@ -3,4 +3,4 @@ SELECT
   grupo_economico,
   total_custo_estoque,
   total_itens_alta_rotatividade
-FROM `rm-farma-dw-prod.licenciado.get_stock_metrics`(@cnpj)
+FROM `rmfarma.ISAZ.get_stock_metrics`(@cnpj)
